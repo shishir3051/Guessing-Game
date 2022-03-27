0c3bb5a387e0c0c558e9d45d6808f868abf2c5fa
Gaziur Rahman Shishir <gaziur35-3051@diu.edu.bd>
2022-03-27 23:45:08 +0600

    guessing game create for practice

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var numberOfWon = 0;
+var numberOfLost = 0;
+
+for (var i = 0; i <= 5; i++) {
+    var guessNumber = Number(prompt("Enter Number 1 to 5 :"));
+
+    var randomNumber = Math.floor(Math.random() * 5) + 1;
+
+    if (guessNumber == randomNumber) {
+        document.write("<br>" + "You Won This Game ! ");
+        numberOfWon++;
+    } else {
+        document.write(
+            "<br>" + "You Lost This Game , Random Number is " + randomNumber
+        ) + "<br>";
+        numberOfLost++;
+    }
+}
+
+document.write("<br>" + "Number of Won : " + numberOfWon + "<br>");
+document.write("Number of Lost : " + numberOfLost);
