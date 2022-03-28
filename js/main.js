d9d144792ba6ee7d0ff9186603126fee032d7cab
Gaziur Rahman Shishir <gaziur35-3051@diu.edu.bd>
2022-03-28 08:16:24 +0600

    bg color changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var numberOfWon = 0;
 var numberOfLost = 0;
 
-for (var i = 0; i <= 5; i++) {
+for (var i = 1; i <= 5; i++) {
     var guessNumber = Number(prompt("Enter Number 1 to 5 :"));
 
-    var randomNumber = Math.floor(Math.random() * 5) + 1;
+    var randomNumber = Math.floor(Math.random() * 5) +1;
 
     if (guessNumber == randomNumber) {
         document.write("<br>" + "You Won This Game ! ");
